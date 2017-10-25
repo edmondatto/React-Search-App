2e9affa6590f516d962c9f5fe4ea287fb22af4d3
Edmond Atto <edmond091@gmail.com>
2017-10-25 08:06:38 +0300

    Add a search form to the Search App

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,1 +1,21 @@
 import React from 'react';
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  Button,
+} from 'react-bootstrap';
+
+const SearchForm = () => (
+  <Form inline>
+    <FormGroup controlId="formInlineEmail">
+      <FormControl type="search" placeholder="Enter something..." />
+    </FormGroup>
+    {' '}
+    <Button type="submit">
+      Search
+    </Button>
+  </Form>
+);
+
+export default SearchForm;
